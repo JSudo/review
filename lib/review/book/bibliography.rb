2916fb256d41c062e0c4a270bd513fb3b314ba6a
Masanori Kado <kdmsnr@gmail.com>
2020-11-04 01:02:19 +0900

    raise ReVIEW::ConfigError if not found libraries

--- lib/review/book/bibliography.rb
+++ lib/review/book/bibliography.rb
@@ -1,6 +1,10 @@
-require 'bibtex'
-require 'citeproc'
-require 'csl/styles'
+begin
+  require 'bibtex'
+  require 'citeproc'
+  require 'csl/styles'
+rescue LoadError
+  # raise ReVIEW::ConfigError inside the class
+end
 
 module ReVIEW
   module Book
@@ -9,6 +13,8 @@ module ReVIEW
         @bibtex = BibTeX.parse(bibfile, filter: :latex)
         @config = config
         format('text')
+      rescue NameError
+        raise ReVIEW::ConfigError, 'not found bibtex libraries. disabled bibtex feature.'
       end
 
       def format(format)
@@ -16,6 +22,8 @@ module ReVIEW
         @citeproc = CiteProc::Processor.new(style: style, format: format)
         @citeproc.import(@bibtex.to_citeproc)
         self
+      rescue NameError
+        raise ReVIEW::ConfigError, 'not found bibtex libraries. disabled bibtex feature.'
       end
 
       def ref(key)
